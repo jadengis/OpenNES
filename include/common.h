c42cc6825210cb62db220a5c3947eba0d9da5693
jadengis <jadengis@gmail.com>
2016-08-23 01:10:19 -0400

    Last commit before branch for work items #1

--- include/common.h
+++ include/common.h
@@ -10,14 +10,39 @@ typedef unsigned short     uint16;
 typedef unsigned int       uint32;
 typedef unsigned long long uint64;
 
-// Common constant definitions
-static const uint64 MAX_SIZE_8BIT = 256;
-static const uint8  NEG_BIT_8BIT  = 0x80;
+// com - The common namespace for all common helper functions and
+//       constant values used throughout the source.
+namespace com {
+  // Common enum type for referencing bit positions
+  enum bits {
+    BIT0 = 0,
+    BIT1,
+    BIT2,
+    BIT3,
+    BIT4,
+    BIT5,
+    BIT6,
+    BIT7
+  };
 
-// Common helper functions
-static inline uint8 Check_Neg_Bit(uint8 x) {
-  // Returns 1 if neg bit 1, 0 otherwise
-  return (reg.AC & NEG_BIT_8BIT) >> 7;
+  // Common constant definitions
+  static const uint64 MAX_SIZE_8BIT = 256;
+  static const uint8  ONE_BIT_MASK  = 0x01;
+
+  // Common helper functions
+  static inline uint8 Check_Zero(uint8 x) {
+    // Returns 1 if 0, 0 otherwise
+    return x == 0;
+  }
+
+  static inline uint8 Check_Not_Zero(uint8 x) {
+    // Returns 1 if not 0, 0 otherwise
+    return x != 0;
+  }
+
+  static inline uint8 Check_Nth_Bit(uint8 x, uint8 N) {
+    // Returns 1 if Nth bit 1, 0 otherwise. Bit indexing it 0 - 7.
+    return (x >> N) & ONE_BIT_MASK;
+  }
 }
-
 #endif // COMMON_H //:~
--- source/mos_6502.cpp
+++ source/mos_6502.cpp
@@ -9,8 +9,14 @@
 //
 #include "cpu_6502.h"
 
+using namespace com;
+
 // ----------------------------------------------------------------------------
 // Instruction emulation functions
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// Add Memory to Accumulator with Carry
 // ----------------------------------------------------------------------------
 void MOS_6502::ADC(const uint8 *M_p) {
   // Store the original value of the accumulator
@@ -26,26 +32,35 @@ void MOS_6502::ADC(const uint8 *M_p) {
   return;
 }
 
+// ----------------------------------------------------------------------------
+// AND Memory with Accumulator
+// ----------------------------------------------------------------------------
 void MOS_6502::AND(const uint8 *M_p) {
   // AND the memory M with the Accumulator
   reg.AC = reg.AC & *M_p;
   // Set the remaining SR flags
   reg.SR.Z = Check_Zero(reg.AC);
-  reg.SR.N = Check_Neg_Bit(reg.AC);
+  reg.SR.N = Check_Nth_Bit(reg.AC, BIT7);
   return;
 }
 
+// ----------------------------------------------------------------------------
+// Shift Left One Bit (Memory or Accumulator)
+// ----------------------------------------------------------------------------
 void MOS_6502::ASL(uint8* M_p) {
   // Set the carry bit.
-  reg.SR.C = Check_Neg_Bit(*M_p);
+  reg.SR.C = Check_Nth_Bit(*M_p, BIT7);
   // Shift memory (or accumulator) left 1
   *M_p = *M_p << 1;
   // Set the remaining SR flags
   reg.SR.Z = (*M_p == 0);
-  reg.SR.N = Check_Neg_Bit(*M_p);
+  reg.SR.N = Check_Nth_Bit(*M_p, BIT7);
   return;
 }
 
+// ----------------------------------------------------------------------------
+// Branch on Carry Clear
+// ----------------------------------------------------------------------------
 void MOS_6502::BCC(uint8* M_p) {
   if(reg.SR.C == 0) {
     // Condition true, branch to PC + offset
@@ -54,9 +69,43 @@ void MOS_6502::BCC(uint8* M_p) {
   return;
 }
 
+// ----------------------------------------------------------------------------
+// Branch of Carry Set
+// ----------------------------------------------------------------------------
 void MOS_6502::BCS(uint8* M_p) {
   if(reg.SR.C == 1) {
     // Condition true, branch to PC + offset
     reg.PC = reg.PC + static_cast<int8>(*M_p);
   }
   return;
+}
+
+// ----------------------------------------------------------------------------
+// Branch on Result Zero
+// ----------------------------------------------------------------------------
+void MOS_6502::BEQ(uint8* M_p) {
+  if(reg.SR.Z == 1) {
+    // Condition true, branch to PC + offset
+    reg.PC = reg.PC + static_cast<int8>(*M_p);
+  }
+  return;
+}
+
+// ----------------------------------------------------------------------------
+// Test Bits in Memory with Accumulator
+// ----------------------------------------------------------------------------
+void MOS_6502::BIT(uint8* M_p) {
+  // zero flag is set to result of A AND M
+  reg.SR.Z = Check_Not_Zero(reg.AC & *M_p);
+  // M7 -> N, M6 -> V
+  reg.SR.N = Check_Nth_Bit(*M_p, BIT7);
+  reg.SR.V = Check_Nth_Bit(*M_p, BIT6);
+  return;
+}
+
+// ----------------------------------------------------------------------------
+// Test Bits in Memory with Accumulator
+// ----------------------------------------------------------------------------
+void MOS_6502::BMI(uint8* M_p) {
+  return;
+}
