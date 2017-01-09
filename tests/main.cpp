21ec2e82a240daf571794e90681eadfd22c7fb54
jadengis <jadengis@gmail.com>
2017-01-08 23:04:22 -0500

    add the Catch main

---
+++ tests/main.cpp
@@ -0,0 +1,15 @@
+//===-- tests/main.cpp - Catch Framework Main File --------------*- C++ -*-===//
+//
+//                           The OsNES Project
+//
+// This file is distributed under GPL v2. See LICENSE.md for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the main functions for running tests within the Catch
+/// framework.
+///
+//===----------------------------------------------------------------------===//
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
