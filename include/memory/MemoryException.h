4ea0915de1f9686ca6258d55878201a868b514a2
jadengis <jadengis@gmail.com>
2017-04-02 02:57:18 -0400

    add more exceptions

--- include/memory/MemoryException.h
+++ include/memory/MemoryException.h
@@ -17,17 +17,40 @@
 
 namespace Exception {
 
+/// \class MemoryException
+/// \brief This the the base exception type for all memory related exceptions.
+class MemoryException : public BaseException {
+  public:
+    MemoryException() : BaseException(
+        "A Memory error occured at runtime.",
+        "MemoryException") {}
+    MemoryException(
+        std::string&& errorMessage,
+        std::string&& className = "MemoryException") :
+      BaseException(errorMessage, className) {}
+    MemoryException(const MemoryException& originalException) noexcept :
+        BaseException(originalException) {}
+
+    // Destructors
+    ~MemoryException() {}
+
+};
+
 /// \class ReadOnlyMemoryException
 /// \brief This is the exception to throw when attempting to write to a read only
 /// memory.
 class ReadOnlyMemoryException : public BaseException {
   public:
     // Construction Methods
-    ReadOnlyMemoryException() : BaseException(
+    ReadOnlyMemoryException() : MemoryException(
         "Attempted write to read only memory at runtime.",
         "ReadOnlyMemoryException") {}
+    ReadOnlyMemoryException(
+        std::string&& errorMessage,
+        std::string&& className = "ReadOnlyMemoryException") :
+      MemoryException(errorMessage, className) {}
     ReadOnlyMemoryException(const ReadOnlyMemoryException& originalException) noexcept :
-        BaseException(originalException) {}
+        MemoryException(originalException) {}
 
     // Destructors
     ~ReadOnlyMemoryException() {}
